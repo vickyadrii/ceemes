b42a37e3617d92596a5947c8f5ba9064066ca937
vickyadrii <vickyadri103@gmail.com>
2025-06-28 00:39:55 +0700

    chore: add toast message when creating, updating and deleting menu groups

--- src/features/settings/menu-group/MenuGroup.tsx
+++ src/features/settings/menu-group/MenuGroup.tsx
@@ -4,6 +4,7 @@ import AddMenuGroup from "./components/AddMenuGroup";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { addMenuGroup, deleteMenuGroup, updateMenuGroup } from "@/store/menuGroupSlice";
 import type { SidebarItemType } from "@/types";
+import { toast } from "sonner";
 
 const MenuGroup = () => {
   const menuGroups = useAppSelector((state) => state.menuGroup);
@@ -11,14 +12,17 @@ const MenuGroup = () => {
 
   const handleAddGroup = (data: SidebarItemType) => {
     dispatch(addMenuGroup({ ...data, children: [] }));
+    toast.success("Menu group added successfully");
   };
 
   const handleEdit = (index: number, updated: SidebarItemType) => {
     dispatch(updateMenuGroup({ index, data: updated }));
+    toast.success("Menu group updated successfully");
   };
 
   const handleDelete = (index: number) => {
     dispatch(deleteMenuGroup(index));
+    toast.success("Menu group deleted successfully");
   };
   return (
     <div className="space-y-6">
--- src/features/settings/menu/components/MenuManager.tsx
+++ src/features/settings/menu/components/MenuManager.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import AddMenu from "./AddMenu";
 import MenuTable from "./MenuTable";
 import type { SidebarItemType } from "@/types";
+import { toast } from "sonner";
 
 type Props = {
   groupIndex: number;
@@ -17,18 +18,21 @@ const MenuManager = ({ groupIndex, group, onUpdateGroup }: Props) => {
     const updatedMenus = [...menus, menu];
     setMenus(updatedMenus);
     onUpdateGroup(groupIndex, { ...group, children: updatedMenus });
+    toast.success("Menu added successfully");
   };
 
   const handleEdit = (index: number, updated: SidebarItemType) => {
     const updatedMenus = menus.map((m, i) => (i === index ? updated : m));
     setMenus(updatedMenus);
     onUpdateGroup(groupIndex, { ...group, children: updatedMenus });
+    toast.success("Menu updated successfully");
   };
 
   const handleDelete = (index: number) => {
     const updatedMenus = menus.filter((_, i) => i !== index);
     setMenus(updatedMenus);
     onUpdateGroup(groupIndex, { ...group, children: updatedMenus });
+    toast.success("Menu deleted successfully");
   };
 
   return (
